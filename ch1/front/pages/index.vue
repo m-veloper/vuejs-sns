b8998a6c35b6b60c7012c890f478092d0f24d5a8
m-veloper <m_veloper@naver.com>
2022-04-20 02:35:06 +0900

    1-8 폼 검증하기

--- ch1/front/pages/index.vue
+++ ch1/front/pages/index.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    Index Page
+    <v-container>
+      <post-card />
+      <post-card />
+      <post-card />
+      <post-card />
+      <post-card />
+    </v-container>
   </div>
 </template>
 
 <script>
+import PostCard from "~/components/PostCard";
 export default {
+  components: {PostCard},
+  comments:{
+    PostCard,
+  },
   data(){
     return {
       name : 'Nuxt.js'
